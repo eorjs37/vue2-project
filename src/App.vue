7ce3eb573ceebc5f5bfd06f551b0ffdcf695b603
CHOI DAE GEON <cdg@aplayz.co.kr>
2025-04-18 08:09:06 +0900

    add : taskqueue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <template>
   <div id="app">
-    <div class="container">
-      <div>
-        <div class="login-form" id="ele">
-          <div class="form-control"><label class="label" for="id">ID</label> <input class="input" type="text" v-model="id" /></div>
-          <div class="form-control"><label class="label" for="password">PASSWORD</label> <input class="input" type="password" v-model="password" /></div>
-          <div class="form-control"><label class="label" for="passwordConfirm">PASSWORD-2</label> <input class="input" type="password" v-model="passwordConfirm" /></div>
-          <div>
-            <span class="error">{{ errors }}</span>
-          </div>
-        </div>
-
-        <div class="ov-hidden mt-3">
-          <button type="button" class="float-right" @click="openModal">Modal</button>
-        </div>
-      </div>
-    </div>
     <router-view></router-view>
+    <video hidden ref="videoPlayer" @ended="onVideoEnded"></video>
+    <button @click="addVideo">비디오 순차진행</button>
     <Modal :visible="modalVisible" @closeModal="onCloseModal"></Modal>
   </div>
 </template>
 
 <script>
-import { list } from '@/api/list';
 import _ from 'lodash';
+import { taskQueue } from './utils/taskQueue';
 export default {
   name: 'App',
   data() {
@@ -35,37 +21,85 @@ export default {
       errors: '',
       coffeeList: [],
       modalVisible: false,
+      currentResolve: null,
+      videoList: ['https://daegeon-everybody.s3.ap-northeast-2.amazonaws.com/video/BigBuckBunny.mp4', 'https://daegeon-everybody.s3.ap-northeast-2.amazonaws.com/video/ElephantsDream.mp4', 'https://daegeon-everybody.s3.ap-northeast-2.amazonaws.com/video/ForBiggerBlazes.mp4'],
     };
   },
 
   /* ######################################### life cycle ######################################### */
   created() {
-    console.log('############ created ############');
-    console.log('dom api ele : ', document.getElementById('ele'));
-    console.log('id : ', this.id);
-    console.log('password : ', this.password);
+    // this.$crontab.addJob({
+    //   name: 'counter1',
+    //   interval: {
+    //     seconds: '/1',
+    //   },
+    //   job: () => {
+    //     taskQueue.add(async () => {
+    //       console.log('[작업1] 시작');
+    //       await this.simulateAsyncTask(3000);
+    //       console.log('[작업1] 완료');
+    //     });
+    //   },
+    // });
+    // this.$crontab.addJob({
+    //   name: 'counter2',
+    //   interval: {
+    //     seconds: '/1',
+    //   },
+    //   job: () => {
+    //     taskQueue.add(async () => {
+    //       console.log('[작업2] 시작');
+    //       await this.simulateAsyncTask(3000);
+    //       console.log('[작업2] 완료');
+    //     });
+    //   },
+    // });
+    // this.$crontab.addJob({
+    //   name: 'counter3',
+    //   interval: {
+    //     seconds: '/1',
+    //   },
+    //   job: () => {
+    //     taskQueue.add(async () => {
+    //       console.log('[작업3] 시작');
+    //       await this.simulateAsyncTask(3000);
+    //       console.log('[작업3] 완료');
+    //     });
+    //   },
+    // });
   },
-  mounted() {
-    console.log('############ mounted ############');
-    console.log('dom api ele : ', document.getElementById('ele'));
-    console.log('id : ', this.id);
-    console.log('password : ', this.password);
-    list().then(response => {
-      const { data } = response;
-      this.coffeeList = data;
-    });
-  },
-
-  updated() {
-    console.log('############ updated ############');
-  },
-
-  destroyed() {
-    console.log('############ destroyed ############');
-  },
+  mounted() {},
 
   /* ######################################### methods ######################################### */
   methods: {
+    addVideo() {
+      this.videoList.forEach(item => {
+        this.enqueueVideo(item);
+      });
+    },
+    enqueueVideo(src) {
+      taskQueue.add(() => this.playVideo(src));
+    },
+    playVideo(src) {
+      return new Promise(resolve => {
+        const video = this.$refs.videoPlayer;
+        this.currentResolve = resolve;
+
+        video.src = src;
+        video.load();
+        video.play();
+      });
+    },
+    onVideoEnded() {
+      if (this.currentResolve) {
+        this.currentResolve(); // 현재 비디오 끝나면 큐 다음 작업 실행
+        this.currentResolve = null;
+      }
+    },
+
+    simulateAsyncTask(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    },
     diffPassword() {
       if (this.password !== this.passwordConfirm) {
         this.errors = '비밀번호가 다릅니다.';
@@ -77,37 +111,7 @@ export default {
       this.modalVisible = true;
     },
     onCloseModal(value) {
-      console.log('value : ', value);
       this.modalVisible = value;
-    },
-  },
-
-  /* ######################################### watch ######################################### */
-  watch: {
-    id(newVal, oldVal) {
-      console.log('newVal : ', newVal);
-      console.log('oldVal : ', oldVal);
-    },
-    password() {
-      this.diffPassword();
-    },
-    passwordConfirm() {
-      this.diffPassword();
-    },
-  },
-
-  /* ######################################### computed ######################################### */
-  computed: {
-    newCoffeeList: {
-      get() {
-        const copyCoffeeList = _.cloneDeep(this.coffeeList);
-
-        copyCoffeeList.forEach(coffee => {
-          coffee['newItem'] = coffee.menuNm + '/' + coffee.price + '원';
-        });
-
-        return copyCoffeeList;
-      },
     },
   },
 };
