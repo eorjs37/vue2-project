28cddb5e9cb0bedc5615386c123b200a93818dc2
CHOI DAE GEON <cdg@aplayz.co.kr>
2025-04-18 08:07:10 +0900

    add : taskQueue

---
+++ src/utils/taskQueue.js
@@ -0,0 +1,31 @@
+class TaskQueue {
+  constructor() {
+    this.queue = [];
+    this.isRunning = false;
+  }
+
+  add(task) {
+    this.queue.push(task);
+    this.runNext();
+  }
+
+  async runNext() {
+    if (this.isRunning || this.queue.length === 0) return;
+
+    this.isRunning = true;
+
+    const task = this.queue.shift();
+    if (task) {
+      try {
+        await task();
+      } catch (error) {
+        console.error('작업 실행 중 에러:', error);
+      }
+    }
+
+    this.isRunning = false;
+    this.runNext();
+  }
+}
+
+export const taskQueue = new TaskQueue();
