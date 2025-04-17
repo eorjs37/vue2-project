714b08e84673b975547907d3bbc0df123568d55a
CHOI DAE GEON <cdg@aplayz.co.kr>
2025-04-18 08:09:38 +0900

    add : store 추가

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Modal from '@/components/Modal';
-
+import VueCronTab from 'vue-crontab';
+import store from './store';
 //router
 import router from '@/router/index';
 
 Vue.config.productionTip = false;
-
+Vue.use(VueCronTab);
 Vue.component('Modal', Modal);
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
